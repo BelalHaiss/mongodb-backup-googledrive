ffe124ec8f1e45ef0ccc6f8aa4349594438e9708
haiss <belalhaiss@gmail.com>
2023-08-07 16:07:31 +0300

    [fix] any exception error will be logged

--- logger.js
+++ logger.js
@@ -3,7 +3,7 @@ const { combine, timestamp } = format;
 
 export const logger = createLogger({
   transports: [
-    new transports.Console(),
+    new transports.Console({ handleExceptions: true }),
     new transports.File({ level: 'info', filename: 'info.log' }),
     new transports.File({ level: 'error', filename: 'error.log' })
   ],
